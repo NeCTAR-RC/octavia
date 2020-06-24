25912d5d2db0d2bca727ee82904269e1cbf7cc96
Sam Morrison <sorrison@gmail.com>
2020-06-24 16:16:12 +1000

    Nectarism: support restricted zones
    2 new settings
    
    nectar/restricted_zones
    nectar/default_availability_zone
    
    default_availability_zone will set the az of the load balancer
    if restricted_zones = True and no AZ specified by the user.
    
    If the project has restricted zones one of these will be chosen
    instead of the default_availabiliy_zone setting.
    
    Change-Id: Ie58621f5807868d90b84f7d1072633961a156634

---
+++ octavia/common/restricted_zones.py
@@ -0,0 +1,64 @@
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from keystoneclient import client as keystone_client
+from oslo_log import log as logging
+
+from octavia.common import cache_utils
+from octavia.common import keystone
+
+
+LOG = logging.getLogger(__name__)
+
+
+ALL_ZONES = 'ALL'
+AZ_CACHE_SECONDS = 60 * 60
+MC = None
+
+
+def _get_cache():
+    global MC
+
+    if MC is None:
+        MC = cache_utils.get_client(expiration_time=AZ_CACHE_SECONDS)
+
+    return MC
+
+
+def reset_cache():
+    """Reset the cache, mainly for testing purposes and update
+
+    availability_zone for host aggregate
+    """
+    global MC
+    MC = None
+
+
+def get_restricted_zones(project_id):
+    cache = _get_cache()
+    cache_key = 'restricted_zones-%s' % project_id
+    zones = cache.get(cache_key)
+    LOG.warning("Found cached restricted zones: %s", zones)
+
+    if not zones:
+        ksession = keystone.KeystoneSession()
+        kclient = keystone_client.Client(session=ksession.get_session())
+        project = kclient.projects.get(project_id)
+        zones = getattr(project, 'compute_zones', ALL_ZONES)
+        cache.set(cache_key, zones)
+
+        LOG.warning("Cached restricted zones: %s", zones)
+    if not zones or zones == ALL_ZONES:
+        return None
+
+    return zones.split(',')
